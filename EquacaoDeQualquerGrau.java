6e97f7cd31c0a132c4e2e4c1b34af9f9870ca683
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2020-12-09 22:41:19 -0300

    Create EquacaoDeQualquerGrau.java

---
+++ EquacaoDeQualquerGrau.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EquacaoDeQualquerGrau{
+
+    public static void main(String[] args) {
+        
+
+    }
+
+    public static int valores(int n){
+
+        
+    }
+}
