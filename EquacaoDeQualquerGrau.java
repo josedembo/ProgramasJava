6f857c557f70147126818835cb29bb59334dfcaf
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2020-12-13 22:16:58 -0300

    Update EquacaoDeQualquerGrau.java

--- EquacaoDeQualquerGrau.java
+++ EquacaoDeQualquerGrau.java
@@ -3,12 +3,33 @@ import java.util.Scanner;
 public class EquacaoDeQualquerGrau{
 
     public static void main(String[] args) {
-        
+    
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("informe o grau da equacao");
+        int grauDaEquacao = leitor.nextInt();
+        int vetor;
+        int coificientes[] = new int[grauDaEquacao];
+
+        = valoresDosCoificintes(grauDaEquacao);
 
     }
 
-    public static int valores(int n){
 
+    public static int valoresDosCoificintes(int n){
+
+        Scanner leitor = new Scanner(System.in);
+
+        int vetor_coif[] = new int[n];
         
+        System.out.println("Informe a seguir os valores dos coefincinetes\n comecando pelo coinfinciente da variavel de maior expoente ate o termo independente");
+        for(int i =0; i<n;i++){
+
+            System.out.println("R:");
+             vetor_coif[i]=leitor.nextInt();
+        }
+
+        return vetor_coif[n];
+    
     }
 }
